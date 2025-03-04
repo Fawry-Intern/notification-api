594c4c110c01e72a31bc96901701a0a72a307419
Muhammad Hussein <muhammadhussein2312@gmail.com>
2025-03-04 23:44:20 +0200

    Initialize Notification-API

---
+++ src/test/java/com/fawry/notificationapi/NotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fawry.notificationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
