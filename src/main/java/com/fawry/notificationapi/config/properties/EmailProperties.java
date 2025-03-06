662e432faf7a24a94d15b50123af3356d6c9e7d9
Muhammad Hussein <muhammadhussein2312@gmail.com>
2025-03-06 12:09:46 +0200

    feat(config): add EmailProperties to manage custom mail configurations

--- src/main/java/com/fawry/notificationapi/config/properties/EmailProperties.java
+++ src/main/java/com/fawry/notificationapi/config/properties/EmailProperties.java
@@ -1,2 +1,57 @@
-package com.fawry.notificationapi.config;public class EmailProperties {
+package com.fawry.notificationapi.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+@ConfigurationProperties(prefix = "custom.mail")
+public class EmailProperties {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private Properties properties = new Properties();
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
 }
