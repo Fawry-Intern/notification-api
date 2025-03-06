811e49ab2303fd5e852ad5b1ae21ec19c8a6980c
Muhammad Hussein <muhammadhussein2312@gmail.com>
2025-03-06 12:10:21 +0200

    feat(config): add EmailConfig to configure JavaMailSender with custom mail properties

--- src/main/java/com/fawry/notificationapi/config/EmailConfig.java
+++ src/main/java/com/fawry/notificationapi/config/EmailConfig.java
@@ -1,2 +1,32 @@
-package com.fawry.notificationapi.config;public class EmailConfig {
+package com.fawry.notificationapi.config;
+
+import com.fawry.notificationapi.config.properties.EmailProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    private final EmailProperties emailProperties;
+
+    public EmailConfig(EmailProperties emailProperties) {
+        this.emailProperties = emailProperties;
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+
+        var mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(emailProperties.getHost());
+        mailSender.setPort(emailProperties.getPort());
+        mailSender.setUsername(emailProperties.getUsername());
+        mailSender.setPassword(emailProperties.getPassword());
+
+        Properties mailProperties = mailSender.getJavaMailProperties();
+        mailProperties.putAll(emailProperties.getProperties());
+        return mailSender;
+    }
 }
