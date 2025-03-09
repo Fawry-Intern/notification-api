1276a28fbf9f8ebe66c477abd26294b81512d6a3
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2025-03-09 16:32:01 +0200

    Close #2 Feature/fawry 2 notification api (#3)
    * feat(Notification Design): Implement Notification Handlers
    
    - Introduced NotificationHandler interface to define a contract for sending notifications.
                                                                                   - Implemented EmailNotificationHandler, PushNotificationHandler, and SMSNotificationHandler services.
                                                                                   - Each handler is marked with @Service and implements sendNotification(NotificationRequest).
                                                                                   - Enhances flexibility and allows future extensions for multiple notification channels.
    
    * feature(Notification): Implement Notification Service
    
    * feat(Factory): Implement Notification Handler Factory
    
    * Resolved merge conflicts in notification-api
    
    ---------
    
    Co-authored-by: Hossam Hatem <7ossam7atem1@gmail.com>

--- src/main/java/com/fawry/notificationapi/service/NotificationService.java
+++ src/main/java/com/fawry/notificationapi/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.fawry.notificationapi.service;
 
 import com.fawry.notificationapi.model.NotificationRequest;
+import com.fawry.notificationapi.model.NotificationType;
 
 public interface NotificationService {
     void sendNotification(NotificationRequest request);
